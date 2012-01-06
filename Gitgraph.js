a435cfca46db58e1588d80f43b08671756e355b7
Paul Bouchon <bouchon@logicalcognition.com>
2012-01-06 13:09:03 -0500

    Rogue semicolon

--- Gitgraph.js
+++ Gitgraph.js
@@ -100,7 +100,7 @@ var Gitgraph = function(args){
 		this.graphContainer = dojo.create('div',{
 			innerHTML:'<img src="http://biganimals.com/wp-content/themes/biganimals/images/loading_transparent_4.gif"/>',
 			style:'border-radius:3px;border:1px solid #E5E5E5;background:white;height:55px;text-align:center;width:'+(this.width+14)+'px'
-		}this.node,'last');
+		},this.node,'last');
 
 		return this.graphContainer;
 	}
